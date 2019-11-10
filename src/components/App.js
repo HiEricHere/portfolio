2cb4ad3e714ef9dda8bc55cad19618d125d80d1f
Eric Huang <ejhuang84@gmail.com>
2019-11-10 18:58:27 +0800

    added links added pics reset css

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import NavBar from "./NavBar/NavBar";
+import About from "./About/About";
+import Projects from "./Projects/Projects";
+import Blog from "./Blog/Blog";
 
 const App = () => {
-  return <h1>i&#39;m alive?</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/blog" component={Blog} />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <>
+      <ul>
+        <li>PixE</li>
+        <li>Heeded</li>
+        <li>StarChaser</li>
+        <li>LazyChef</li>
+      </ul>
+    </>
+  );
+};
+
+export default Projects;
